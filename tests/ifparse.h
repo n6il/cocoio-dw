2facd16049c4da6ef3093c2c87ecaa7bbc0301f8
Michael R. Furman <n6il@ocs.net>
2022-05-22 11:48:48 -0700

    Change struct order to match hardware registers

--- tests/ifparse.h
+++ tests/ifparse.h
@@ -4,12 +4,12 @@
 #include <stdint.h>
 
 struct w51info {
-    uint8_t iface[8];
-    uint8_t address[4];
     uint8_t gateway[4];
     uint8_t netmask[4];
     uint8_t macaddr[6];
+    uint8_t address[4];
     uint8_t *phyaddr;
+    uint8_t iface[8];
 };
 
 int parseip();
